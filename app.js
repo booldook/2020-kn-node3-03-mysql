f322ab4eb27d312890163ed203c72f1b74f44573
booldook <booldook@naver.com>
2020-05-03 13:50:37 +0900

    sql

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
-const path = reqire('path');
+const path = require('path');
+const mysql = require('mysql2');
+const moment = require('moment');
+
+/* Mysql */
+const connect = mysql.createConnection({
+	host: "localhost",
+	port: 3307,
+	database: "node",
+	user: "root",
+	password: "000000",
+});
 
 /* View engine */
 app.set('view engine', 'pug');
@@ -16,3 +27,11 @@ app.listen(3000, () => {
 });
 
 /* Routers */
+app.get("/insert", (req, res, next) => {
+	let sql = 'INSERT INTO sample SET username=?, age=?, comment=?, created=?';
+	let values = ['홍길룡', 34, '누이를 누이라...', moment().format('YYYY-MM-DD HH:mm:ss')];
+	connect.execute(sql, values, (err, result) => {
+		if(err) res.json(err);
+		else res.json(result);
+	});
+});
